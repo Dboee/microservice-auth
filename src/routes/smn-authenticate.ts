ef989af52f9a51f2d9ec0c28021a5fdd984fb9f0
Dboee <davidboee@gmail.com>
2023-02-11 12:28:35 +0100

    comments out work in progress code

--- src/routes/smn-authenticate.ts
+++ src/routes/smn-authenticate.ts
@@ -1,30 +1,30 @@
-import express, { Request, Response } from 'express';
-import { BadRequestError } from '../errors/bad-request-error';
-import { User } from '../models/user';
-import { SMNAuthenticationService } from './services/SMNAuthentionService';
+// import express, { Request, Response } from 'express';
+// import { BadRequestError } from '../errors/bad-request-error';
+// import { User } from '../models/user';
+// import { SMNAuthenticationService } from './services/SMNAuthentionService';
 
-const router = express.Router();
+// const router = express.Router();
 
-router.post('/smn-authenticate', async (req: Request, res: Response) => {
-  const { email, password } = req.body;
+// router.post('/smn-authenticate', async (req: Request, res: Response) => {
+//   const { email, password } = req.body;
 
-  const existingUser = await User.findOne({ email });
-  if (!existingUser) {
-    throw new BadRequestError('Invalid credentials');
-  }
+//   const existingUser = await User.findOne({ email });
+//   if (!existingUser) {
+//     throw new BadRequestError('Invalid credentials');
+//   }
 
-  const isPasswordMatch = await existingUser.comparePassword(password);
-  if (!isPasswordMatch) {
-    throw new BadRequestError('Invalid credentials');
-  }
+//   const isPasswordMatch = await existingUser.comparePassword(password);
+//   if (!isPasswordMatch) {
+//     throw new BadRequestError('Invalid credentials');
+//   }
 
-  const smnAuthenticationService = new SMNAuthenticationService();
-  const token = await smnAuthenticationService.authenticate(email, password);
+//   const smnAuthenticationService = new SMNAuthenticationService();
+//   const token = await smnAuthenticationService.authenticate(email, password);
 
-  existingUser.smnToken = token;
-  await existingUser.save();
+//   existingUser.smnToken = token;
+//   await existingUser.save();
 
-  res.send({ message: 'SMN authentication successful' });
-});
+//   res.send({ message: 'SMN authentication successful' });
+// });
 
-export { router as smnAuthenticationRouter };
+// export { router as smnAuthenticationRouter };
