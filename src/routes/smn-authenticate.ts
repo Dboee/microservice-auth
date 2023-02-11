b936ceef3dc5d6371e62d50a5e48bf7ed8c87d8a
Dboee <davidboee@gmail.com>
2023-02-11 12:19:52 +0100

    restores old testfiles

---
+++ src/routes/smn-authenticate.ts
@@ -0,0 +1,30 @@
+import express, { Request, Response } from 'express';
+import { BadRequestError } from '../errors/bad-request-error';
+import { User } from '../models/user';
+import { SMNAuthenticationService } from './services/SMNAuthentionService';
+
+const router = express.Router();
+
+router.post('/smn-authenticate', async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  const existingUser = await User.findOne({ email });
+  if (!existingUser) {
+    throw new BadRequestError('Invalid credentials');
+  }
+
+  const isPasswordMatch = await existingUser.comparePassword(password);
+  if (!isPasswordMatch) {
+    throw new BadRequestError('Invalid credentials');
+  }
+
+  const smnAuthenticationService = new SMNAuthenticationService();
+  const token = await smnAuthenticationService.authenticate(email, password);
+
+  existingUser.smnToken = token;
+  await existingUser.save();
+
+  res.send({ message: 'SMN authentication successful' });
+});
+
+export { router as smnAuthenticationRouter };
---
+++ src/services/SMNAuthentionService.ts
@@ -0,0 +1,76 @@
+const express = require('express');
+const axios = require('axios');
+const qs = require('qs');
+
+class SMNAuthenticationService {
+  constructor({ clientId, clientSecret, redirectUri, financialInstitution }) {
+    this.clientId = clientId;
+    this.clientSecret = clientSecret;
+    this.redirectUri = redirectUri;
+    this.financialInstitution = financialInstitution;
+
+  async authenticateUser(req, res) {
+    const state = Math.floor(Math.random() * 100000000).toString();
+    const authorizeUrl = `https://api-auth.sparebank1.no/oauth/authorize?client_id=${this.clientId}&state=${state}&redirect_uri=${this.redirectUri}&finInst=${this.financialInstitution}&response_type=code`;
+
+    // 1. Open a browser window for user authentication and authorization
+    res.redirect(authorizeUrl);
+
+    // 2. Handle the redirect from the authorization server
+    const app = express();
+    app.get(this.redirectUri, async (req, res) => {
+      const { code } = req.query;
+      const response = await axios.post(
+        'https://api-auth.sparebank1.no/oauth/token',
+        qs.stringify({
+          client_id: this.clientId,
+          client_secret: this.clientSecret,
+          code,
+          grant_type: 'authorization_code',
+          state: req.query.state,
+          redirect_uri: this.redirectUri,
+        }),
+        {
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+        }
+      );
+
+      // 3. Extract the access token and refresh token
+      const { access_token, refresh_token } = response.data;
+
+      // 4. Save the access token and refresh token to the user in the database
+      // (implementation omitted)
+
+      res.send('Authentication complete');
+    });
+  }
+
+  async refreshAccessToken(refreshToken) {
+    const response = await axios.post(
+      'https://api-auth.sparebank1.no/oauth/token',
+      qs.stringify({
+        client_id: this.clientId,
+        client_secret: this.clientSecret,
+        refresh_token,
+        grant_type: 'refresh_token',
+      }),
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      }
+    );
+
+    // Extract the new access token and refresh token
+    const { access_token, refresh_token: newRefreshToken } = response.data;
+
+    // Save the new access token and refresh token to the user in the database
+    // (implementation omitted)
+
+    return { access_token, refresh_token: newRefreshToken };
+  }
+}
+
+module.exports = SMNAuthenticationService;
